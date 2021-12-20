6b273d22268432e3aeed165e21d6741e91e84500
Brian Pfeil <brian.pfeil@gmail.com>
2021-12-20 15:56:34 -0500

    adds

--- README.md
+++ README.md
@@ -12,9 +12,13 @@ learn [Rust](https://www.rust-lang.org/) programming language
 # run on file(s) change (ensure `nodemon` is installed)
 # turn off dead_code warning
 nodemon --exec "RUSTFLAGS=\"$RUSTFLAGS -A dead_code\" cargo run" src/main.rs
+
+# watch tests (run `cargo test` when any file changes)
+cargo watch -x check -x test
 ```
 
 ## Resources
 
 * [The Rust Programming Language book](https://doc.rust-lang.org/stable/book/)
 * [Rust by Example](https://doc.rust-lang.org/rust-by-example/)
+* [GitHub - watchexec/cargo-watch: Watches over your Cargo project&#39;s source.](https://github.com/watchexec/cargo-watch)
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(warnings, unused)]
+
 use rand::Rng;
 use std::io;
+
+use futures::prelude::*;
+use tokio::prelude::*;
 
 fn hello() {
     let name = "brian";
@@ -47,10 +54,41 @@ fn struct_example() {
     println!("{:?}", p);
 }
 
+// async fn app() -> Result<()> {
+//     //todo!()
+//     future::ok("Hello world".to_string()).await
+// }
+
+// fn async_example_start() {
+//     let mut rt = tokio::runtime::Runtime::new().unwrap();
+//     let future = app();
+//     rt.block_on(future);
+// }
+
+#[test]
+fn test_eample() {
+
+    assert!(true);
+    assert_eq!(1,1)
+}
+
+#[test]
+fn test_checked() {
+    let i: i32 = 3;
+    let product = i.checked_mul(2).unwrap();
+    assert_eq!(product, 6);
+    let a: i32 = 2;
+    assert_eq!(a.checked_add(3), Some(5))
+}
+
 fn main() {
+    use futures::Future;
+    //async_example_start();
+    // todo!();
     // hello();
     // for_loop();
     // user_input();
     // generate_random_number();
     struct_example();
+    
 }
